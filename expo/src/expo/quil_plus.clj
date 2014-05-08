8f770f44c922853ba77eed660fd6e8e0b328aa40
timsgardner <timsgardner@gmail.com>
2014-05-08 10:04:56 -0400

    quil-plus better organized, kruft removed

--- expo/src/expo/quil_plus.clj
+++ expo/src/expo/quil_plus.clj
@@ -6,6 +6,8 @@
             [serializable.fn :as sfn])
   (:use [clojure.pprint :only [pprint, print-table]]))
 
+;; setup ----------------------------------------------------------
+
 ;; absorb quil.core:
 (let [qcvars (->> 'quil.core
                   find-ns
@@ -18,6 +20,9 @@
 ;; absorb serializable.fn:
 (potemkin/import-vars [serializable.fn fn])
 
+
+;; printing conveniences ------------------------------------------
+
 (defmacro prinlim [{:keys [level length] :or {level 5 length 5}}
                    & body]
   `(binding [*print-level* ~level
@@ -28,23 +33,12 @@
   (doseq [x (sort (map str xs))]
     (println x)))
 
-(defmacro var-catcher [x]
-  (or (when (or (not (symbol? x))
-                (find &env x))
-        x)
-      (when-let [v (find-var (symbol (name (ns-name *ns*)) (name x)))]
-        `(quote ~v))
-      x))
-
-(def frame-timeout 1000)
-
-(defn call-all [fs]
-  (doseq [f fs] (f)))
-
 (defmacro printval [& forms]
   (cons 'do
         (for [form forms]
           `(println (str ~(str form) " = " ~form)))))
+
+;; repl interaction  ----------------------------------------------
 
 (declare push-do)
 
@@ -53,41 +47,21 @@
      (push-do #(deliver p (f)))
      @p))
 
+;; mouse conveniences ---------------------------------------------
+
 (defmacro let-mouse [[x y] & body]
   `(let [~x (mouse-x)
          ~y (mouse-y)]
      ~@body))
 
+;; exception state ------------------------------------------------
+
 (def exception-storage (atom [] :validator (fn [x] (every? #(instance? java.lang.Exception %) x))))
 
 (defn flush-exception-storage [] (reset! exception-storage []))
 
-(defprotocol IRevisable
-  (revise [this]))
+;; vocabulary for interacting with Processing settings ------------
 
-(defrecord RevisableVersion [ns original-name value]
-  IRevisable
-  (revise [this]
-    (if original-name
-      (let [revision (var-get ((ns-map ns) original-name))]
-        (assoc this :value revision))
-      this)))
-
-(defn get-names [value]
-  (let [m (ns-map *ns*)]
-    (->> (group-by m (keys m))
-         (reduce-kv (fn [bldg k v]
-                      (-> bldg (assoc (var-get k) v)))
-                     (transient {}))
-         persistent!)))
-
-
-(declare change-procedure)
-
-(defn commit [] (change-procedure #(mapv revise %)))
-
-
-;; vocabulary for interacting with Processing
 (def shape-modes #{:corner :corners
                    :radius :center})
 (def stroke-caps #{:square :project :round})
@@ -96,82 +70,88 @@
                              stroke-caps
                              negs))
 
+(defn qwik-set [& params]
+  (do (doseq [[k v] (partition 2 (remove mono-syms params))]
+        (case k
+          :s    (if (coll? v)
+                  (apply stroke v)
+                  (stroke v))
+          :sw   (if (coll? v)
+                  (apply stroke-weight v)
+                  (stroke-weight v))
+          :f    (if (coll? v)
+                  (apply fill v)
+                  (fill v))
+          :font (if (coll? v)
+                  (apply text-font v)
+                  (text-font v))
+          :fs   (text-size v)))
+      (doseq [x (filter mono-syms params)]
+        (cond
+         (shape-modes x) (do (ellipse-mode x)
+                             (rect-mode x))
+         (stroke-caps x) (stroke-cap x)
+         (= :ns x)       (no-stroke)
+         (= :nf x)       (no-fill)))))
+
+(defmacro qwik-set*
+  "macro version of qwik-set. Faster."
+  [& params]
+  (let [unpack       (fn [f v] (cond
+                               (vector? v) `(~f ~@v)
+                               (list? v) ~v
+                               :else     `(~v)))
+        exprs-1 (for [[k v] (partition 2 (remove mono-syms params))]
+                  (case k
+                    :s `(stroke ~(unpack 'color v))
+                    :sw `(stroke-weight ~v)
+                    :f `(fill ~(unpack 'color v))
+                    :font (if (vector? v)
+                            `(text-font ~@v)
+                            `(text-font ~v))
+                    :fs `(text-size ~v)))
+        exprs-2 (for [x (filter mono-syms params)]
+                  (cond
+                   (shape-modes x) `(do (ellipse-mode ~x)
+                                        (rect-mode ~x))
+                   (stroke-caps x) `(stroke-cap ~x)
+                   (= :ns x) `(no-stroke)
+                   (= :nf x) `(no-fill)))]
+    (cons 'do (concat exprs-1 exprs-2))))
+
+(defmacro matrix-block [& body]
+  `(do (push-matrix)
+       (let [return# (do ~@body)]
+         (pop-matrix)
+         return#)))
+
+(defmacro px-block [pxs-name & body]
+  `(let [~pxs-name (pixels)
+         ret# (do ~@body)]
+     (update-pixels)
+     ret#))
+
+(defmacro shape-block [& body]
+  `(do (begin-shape)
+       (let [return# (do ~@body)]
+         (end-shape)
+         return#)))
+
+;; cumulative live coding -----------------------------------------
+
 ;; top-level let-over-lambda's kind of obnoxious,
 ;; but we really don't want to be worrying about
 ;; messing up this piece of state while live coding
 
+
 (let [do-vec (atom [])]
-  
+
   (defn push-do [f]
     (swap! do-vec #(conj % f)))
-  
-  (defn qwik-set [& params]
-    (do (doseq [[k v] (partition 2 (remove mono-syms params))]
-          (case k
-            :s    (if (coll? v)
-                    (apply stroke v)
-                    (stroke v))
-            :sw   (if (coll? v)
-                    (apply stroke-weight v)
-                    (stroke-weight v))
-            :f    (if (coll? v)
-                    (apply fill v)
-                    (fill v))
-            :font (if (coll? v)
-                    (apply text-font v)
-                    (text-font v))
-            :fs   (text-size v)))
-        (doseq [x (filter mono-syms params)]
-          (cond
-           (shape-modes x) (do (ellipse-mode x)
-                               (rect-mode x))
-           (stroke-caps x) (stroke-cap x)
-           (= :ns x)       (no-stroke)
-           (= :nf x)       (no-fill)))))
-  
-  (defmacro qwik-set* [& params]
-    (let [unpack       (fn [f v] (cond
-                                 (vector? v) `(~f ~@v)
-                                 (list? v) ~v
-                                 :else     `(~v)))
-          exprs-1 (for [[k v] (partition 2 (remove mono-syms params))]
-                    (case k
-                      :s `(stroke ~(unpack 'color v))
-                      :sw `(stroke-weight ~v)
-                      :f `(fill ~(unpack 'color v))
-                      :font (if (vector? v)
-                              `(text-font ~@v)
-                              `(text-font ~v))
-                      :fs `(text-size ~v)))
-          exprs-2 (for [x (filter mono-syms params)]
-                    (cond
-                     (shape-modes x) `(do (ellipse-mode ~x)
-                                          (rect-mode ~x))
-                     (stroke-caps x) `(stroke-cap ~x)
-                     (= :ns x) `(no-stroke)
-                     (= :nf x) `(no-fill)))]
-      (cons 'do (concat exprs-1 exprs-2))))
 
-  (defmacro matrix-block [& body]
-    `(do (push-matrix)
-         (let [return# (do ~@body)]
-           (pop-matrix)
-           return#)))
-
-  (defmacro px-block [pxs-name & body]
-    `(let [~pxs-name (pixels)
-           ret# (do ~@body)]
-       (update-pixels)
-       ret#))
-
-  (defmacro shape-block [& body]
-    `(do (begin-shape)
-         (let [return# (do ~@body)]
-           (end-shape)
-           return#)))
-
-  (let [history-atm (atom [[#(background 0)]] :validator #(and (vector? %)
-                                                               (every? vector? %)))
+  (let [history-atm (atom [[#(background 0)]]
+                          :validator #(and (vector? %)
+                                           (every? vector? %)))
         present-atm (atom 0)
         paused-atm (atom false)]
 
@@ -218,6 +198,14 @@
     
     (defn change-procedure [f]
       (put-procedure (f (procedure))))
+
+    (defmacro var-catcher [x]
+      (or (when (or (not (symbol? x))
+                    (find &env x))
+            x)
+          (when-let [v (find-var (symbol (name (ns-name *ns*)) (name x)))]
+            `(quote ~v))
+          x))
     
     (defmacro push-fn [f]
       `(put-procedure
@@ -262,7 +250,7 @@
                  (catch java.lang.RuntimeException e
                    (println (frmt i "\"Unreadable form (probably unserializable fn)\""))))))
         (println
-         (str index " is a shitty index:\n"
+         (str index " is a bad index:\n"
               (with-out-str (printval (count @history-atm)))
               "\n"))))
     
