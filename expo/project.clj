dcb6e99d613dea54d0aefb8e541cb110665f369e
timsgardner <timsgardner@gmail.com>
2014-05-07 21:46:35 -0400

    Updating project.clj

--- expo/project.clj
+++ expo/project.clj
@@ -1,6 +1,7 @@
 (defproject expo "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Exposition of some simple stuff"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [riddley "0.1.7"]
+                 [org.clojure/core.match "0.2.1"]])
